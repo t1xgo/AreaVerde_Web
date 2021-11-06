746aba8ecc859ba7df7effb2a4d65317426c57c9
Yesenia López Giraldo <yesenialopezg07@gmail.com>
2021-11-06 13:12:06 -0500

    Validacion del cambio de categoria a recolectores

--- green_area/src/views/AdminPanel.vue
+++ green_area/src/views/AdminPanel.vue
@@ -253,7 +253,14 @@
                     </v-col>
                     <v-row>
                       <v-col sm="6" cols="6">
-                        <v-btn color="green" @click="actualizarRecolector" dark tile> Actualizar </v-btn>
+                        <v-btn
+                          color="green"
+                          @click="actualizarRecolector"
+                          dark
+                          tile
+                        >
+                          Actualizar
+                        </v-btn>
                       </v-col>
                       <v-col sm="6" cols="6">
                         <v-btn
@@ -272,7 +279,6 @@
             </div>
           </v-col>
         </v-row>
-
       </v-window-item>
 
       <!-- ...............................................Seccion de administracion de reportes........................................................-->
@@ -351,6 +357,7 @@
 
 <script>
 import axios from "axios";
+import Swal from 'sweetalert2/src/sweetalert2.js';
 export default {
   recolectorSeleccionado: "",
   Cambiocategoria: "",
@@ -490,11 +497,49 @@ export default {
     };
   },
   methods: {
-    async actualizarRecolector(){
-      console.log(this.recolectorSeleccionado.id_personal)
-      console.log(this.Cambiocategoria)
+    reloadPage() {
+      window.location.reload();
     },
-    RecolectorSele(colls){
+    async actualizarRecolector() {
+      console.log(this.recolectorSeleccionado.id_personal);
+      console.log(this.Cambiocategoria);
+      if (
+        this.recolectorSeleccionado.id_personal != undefined &&
+        this.recolectorSeleccionado.id_personal != "" &&
+        this.Cambiocategoria != undefined &&
+        this.Cambiocategoria != ""
+      ) {
+        let token = this.token;
+        let recolector = {
+          id_personal: this.recolectorSeleccionado.id_personal,
+          id_categoriarecolector: "",
+        };
+        if (this.Cambiocategoria == "Reciclable") {
+          recolector.id_categoriarecolector = 1;
+        } else if (this.Cambiocategoria == "Organico") {
+          recolector.id_categoriarecolector = 2;
+        } else if (this.Cambiocategoria == "No reciclable") {
+          recolector.id_categoriarecolector = 3;
+        }
+        await axios.put("http://localhost:3001/putrecolectores", recolector, {
+          headers: { token },
+        });
+        Swal.fire({
+          icon: "success",
+          title: "Ok...",
+          text: "Categoria actualizada",
+        });
+        this.reloadPage();
+      } else {
+        console.log("Variables indefinidas");
+        Swal.fire({
+          icon: "error",
+          title: "Ups...",
+          text: "Recolector o Categoría indefinidos",
+        });
+      }
+    },
+    RecolectorSele(colls) {
       this.showingReportsModal = true;
       this.recolectorSeleccionado = colls;
     },
@@ -504,7 +549,6 @@ export default {
         headers: { token },
       });
       this.collectors = response.data.content;
-      console.log("RESPONSEE", response.data.content);
     },
     async crearRecolecor() {
       let tipo;
@@ -523,14 +567,10 @@ export default {
         id_categoriarecolector: tipo,
       };
       let token = this.token;
-      let response = await axios.post(
-        "http://localhost:3001/postrecolectores",
-        recolector,
-        {
-          headers: { token },
-        }
-      );
-      console.log(response.data);
+      await axios.post("http://localhost:3001/postrecolectores", recolector, {
+        headers: { token },
+      });
+      this.reloadPage();
     },
   },
 };
